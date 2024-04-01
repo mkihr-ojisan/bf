31271a49ea337f50f8cbb0e97c13a8be6934d77e
mkihr-ojisan <mkihr.ojisan@gmail.com>
2024-04-01 13:50:00 +0900

    Add description to help

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,19 @@ mod runtime;
 
 #[derive(Parser)]
 struct Args {
-    #[clap(short = 'O', long, default_value = "2", value_parser = 0..=2)]
+    /// Optimization level (0-2)
+    #[clap(short = 'O', long, default_value = "0", value_parser = 0..=2)]
     optimize_level: i64,
+    /// Print optimized intermediate representation
     #[clap(long)]
     print_optimized: bool,
+    /// Trace execution. Only works if not using native compilation
     #[clap(long)]
     trace: bool,
-    #[clap(long, default_value = "true")]
+    /// Enable native compilation
+    #[clap(long, default_value = "false")]
     native_compile: bool,
+    /// Input file. If not provided, reads from stdin
     filename: Option<String>,
 }
 
